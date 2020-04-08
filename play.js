b27924dee179df6c26d066b48aedc8b43fce49a0
Stacey Keating <staceyamberkk@gmail.com>
2020-04-08 21:45:59 +0000

    stdin data and exit key added

--- play.js
+++ play.js
@@ -1,3 +1,16 @@
 const connect = require('./client');
 console.log('Connecting ...');
 connect();
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  handleUserInput= (data) => {
+    if (key === '\u0003') 
+      stdin.exit();
+    }
+  return stdin;
+}
+
